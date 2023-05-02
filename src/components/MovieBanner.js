09f4fcf6f6d0d9ca16a2a2a68afd12f03ef7bdb8
Yui Sakamoto <yui.sakamoto11@gmail.com>
2023-05-01 18:32:20 -0700

    added source set to banner img

--- src/components/MovieBanner.js
+++ src/components/MovieBanner.js
@@ -3,10 +3,14 @@ import MoreInfoButton from "./MoreInfoButton"
 function MovieBanner({movies}) {
   return (
     <div className='banner-container'>
-      <img src={`https://image.tmdb.org/t/p/original/${movies[0].backdrop_path}`} alt={`${movies[0].title} Poster`} />
+      <picture>
+        <source media="(max-width: 400px)" srcSet={`https://image.tmdb.org/t/p/w780/${movies[0].backdrop_path}`}/>
+        <source media="(max-width: 720px)" srcSet={`https://image.tmdb.org/t/p/w1280/${movies[0].backdrop_path}`}/>
+        <img src={`https://image.tmdb.org/t/p/original/${movies[0].backdrop_path}`} alt={`${movies[0].title} Poster`} />
+      </picture>
       <div className="banner-info-container">
         <h1>{movies[0].title}</h1>
-        <h2>{movies[0].release_date}</h2>
+        {/* <h2>{movies[0].release_date}</h2> */}
         <MoreInfoButton movies={movies[0]}/>
       </div>
     </div>
