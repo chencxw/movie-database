fce6155e9395ddcca8e1a4e31fc48b5e148d9981
Brice Chen <bchen72@my.bcit.ca>
2023-05-07 12:15:01 -0700

    testing favourites

--- src/pages/PageFavourite.js
+++ src/pages/PageFavourite.js
@@ -1,28 +1,33 @@
-import { useEffect } from 'react';
-import { appTitle } from '../globals/globals';
-import { useSelector } from 'react-redux';
-import MovieCard from '../components/MovieCard';
-import {Link} from 'react-router-dom';
+import { useEffect } from "react";
+import { appTitle } from "../globals/globals";
+import { useSelector } from "react-redux";
+import MovieCard from "../components/MovieCard";
+import { Link } from "react-router-dom";
 
 function PageFavourite() {
-
   const favs = useSelector((state) => state.favs.items);
-  
+
   useEffect(() => {
     document.title = `${appTitle} - Favourites`;
-  }, [])
+  }, []);
 
   return (
     <main>
       <section className="favourites">
         <h2>Favourites</h2>
-        {favs.length < 1 ? <p>No movies added to favourites yet. Go back to the <Link to="/">home</Link> page to add some.</p>:
-        <div className="favs-grid">
-          <MovieCard  movies={favs} isFav={true} />
-        </div>}
+        {favs.length < 1 ? (
+          <p>
+            No movies added to favourites yet. Go back to the{" "}
+            <Link to="/">home</Link> page to add some.
+          </p>
+        ) : (
+          <div id="favs-grid">
+            <MovieCard movies={favs} isFav={true} />
+          </div>
+        )}
       </section>
     </main>
-  ) 
+  );
 }
 
 export default PageFavourite;
