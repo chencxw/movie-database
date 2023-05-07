ea1a17f81beaff1e9c35f76aceeae2ac6acea6ac
Crystal Chen <cchen435@my.bcit.ca>
2023-05-06 17:12:55 -0700

    styled about page

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { appTitle } from '../globals/globals';
+import tmbdLogo from '../images/tmbdLogo.svg';
 
 
 function PageAbout() {
@@ -11,12 +12,14 @@ function PageAbout() {
   return (
     <main>
       <section className="about">
-        <h2>About</h2>
+        <h2>Welcome to Film Box!</h2>
         <article>
           <p>Film box movie database is a website where users can find popular, top rated, now playing and upcoming movie listings. Browse for your favourite genres or movies, check out their ratings and see how they match up! </p>
 
+          <p>Found something you like? Add them to your favourites and remove them anytime.</p>
+
           <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
-          {/* TMDb logo - <img src="" alt="" /> */}
+          <img src={tmbdLogo} alt="TMBD Logo" />
         </article>
       </section>
     </main>
