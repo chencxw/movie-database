2fb99c44436303b491cedba6e9962f85d98bf9a5
Crystal Chen <cchen435@my.bcit.ca>
2023-05-07 21:04:03 -0700

    edited padding on favourites page and edited bottom gradient on banner image

--- src/components/MovieBanner.js
+++ src/components/MovieBanner.js
@@ -26,6 +26,7 @@ function MovieBanner({movies}) {
         <source media="(max-width: 400px)" srcSet={`https://image.tmdb.org/t/p/w780/${movies[0].backdrop_path}`}/>
         <source media="(max-width: 720px)" srcSet={`https://image.tmdb.org/t/p/w1280/${movies[0].backdrop_path}`}/>
         <img src={`https://image.tmdb.org/t/p/original/${movies[0].backdrop_path}`} alt={`${movies[0].title} Poster`} />
+        <div className="bottom-gradient"></div>
       </picture>
       <div className="banner-info-container">
         <h1>{movies[0].title}</h1>
