d343e0d9e9d6b3938c4364348dd1b71e7c1d0b7c
Yui Sakamoto <yui.sakamoto11@gmail.com>
2023-05-09 09:08:35 -0700

    fixed an error in nav link

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -9,25 +9,25 @@ function FilterButtons() {
     <nav className="nav-sort">
       <ul>
         <li>
-          <NavLink exact to="/sort/popular" className={({ isActive }) => (isActive || location.pathname === '/' ? 'active' : undefined)}>
+          <NavLink to="/sort/popular" className={({ isActive }) => (isActive || location.pathname === '/' ? 'active' : undefined)}>
             Popular
           </NavLink>
         </li>
         <span>|</span>
         <li>
-          <NavLink exact to="/sort/top-rated" className={({ isActive }) => (isActive ? 'active' : undefined)}>
+          <NavLink to="/sort/top-rated" className={({ isActive }) => (isActive ? 'active' : undefined)}>
             Top Rated
           </NavLink>
         </li>
         <span>|</span>
         <li>
-          <NavLink exact to="/sort/now-playing" className={({ isActive }) => (isActive ? 'active' : undefined)}>
+          <NavLink to="/sort/now-playing" className={({ isActive }) => (isActive ? 'active' : undefined)}>
             Now Playing
           </NavLink>
         </li>
         <span>|</span>
         <li>
-          <NavLink exact to="/sort/upcoming" className={({ isActive }) => (isActive ? 'active' : undefined)}>
+          <NavLink to="/sort/upcoming" className={({ isActive }) => (isActive ? 'active' : undefined)}>
             Upcoming
           </NavLink>
         </li>
