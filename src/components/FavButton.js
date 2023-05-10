ba5b7864b560be8d9fcc373ce0218415e39a68a9
Brice Chen <bchen72@my.bcit.ca>
2023-05-09 20:05:08 -0700

    add the propogation thing

--- src/components/FavButton.js
+++ src/components/FavButton.js
@@ -1,35 +1,49 @@
 function FavButton({ movies, remove, handleFavClick }) {
-
-  function handleAddFav(){
+  function handleAddFav(event) {
+    // event.stopPropagation();
     handleFavClick(true, movies);
   }
 
-  function handleRemoveFav(){
+  function handleRemoveFav(event) {
+    // event.stopPropagation();
     handleFavClick(false, movies);
   }
 
   return (
     <>
-      {remove === false ? 
+      {remove === false ? (
         <button onClick={handleAddFav}>
-          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
-              <path d="M11.5 0c6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5-11.5-5.153-11.5-11.5 5.153-11.5 11.5-11.5zm0 1c5.795 0 10.5 4.705 10.5 10.5s-4.705 10.5-10.5 10.5-10.5-4.705-10.5-10.5 4.705-10.5 10.5-10.5zm.5 10h6v1h-6v6h-1v-6h-6v-1h6v-6h1v6z"/>
+          <svg
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            fillRule="evenodd"
+            clipRule="evenodd"
+          >
+            <path d="M11.5 0c6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5-11.5-5.153-11.5-11.5 5.153-11.5 11.5-11.5zm0 1c5.795 0 10.5 4.705 10.5 10.5s-4.705 10.5-10.5 10.5-10.5-4.705-10.5-10.5 4.705-10.5 10.5-10.5zm.5 10h6v1h-6v6h-1v-6h-6v-1h6v-6h1v6z" />
           </svg>
           <span>Add to Favourites</span>
-      </button> : 
-      <button onClick={handleRemoveFav}>
-          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
-              <path d="M11.5 0c6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5-11.5-5.153-11.5-11.5 5.153-11.5 11.5-11.5zm0 1c5.795 0 10.5 4.705 10.5 10.5s-4.705 10.5-10.5 10.5-10.5-4.705-10.5-10.5 4.705-10.5 10.5-10.5zm-6.5 10h13v1h-13v-1z"/>
+        </button>
+      ) : (
+        <button onClick={handleRemoveFav}>
+          <svg
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            fillRule="evenodd"
+            clipRule="evenodd"
+          >
+            <path d="M11.5 0c6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5-11.5-5.153-11.5-11.5 5.153-11.5 11.5-11.5zm0 1c5.795 0 10.5 4.705 10.5 10.5s-4.705 10.5-10.5 10.5-10.5-4.705-10.5-10.5 4.705-10.5 10.5-10.5zm-6.5 10h13v1h-13v-1z" />
           </svg>
           <span>Unfavourite</span>
-      </button>}
+        </button>
+      )}
     </>
   );
-    
 }
 
 FavButton.defaultProps = {
-    remove: false
-}
+  remove: false
+};
 
 export default FavButton;
