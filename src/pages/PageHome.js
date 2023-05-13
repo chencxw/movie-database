67a5f35999893a0144b32acdca85ead59197718a
Crystal Chen <cchen435@my.bcit.ca>
2023-05-13 11:21:45 -0700

    edited scroll for changing pages

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import { appTitle, API_TOKEN } from "../globals/globals";
 import MovieCard from "../components/MovieCard";
 import MovieBanner from "../components/MovieBanner";
+import FilterMobile from '../components/FilterMobile';
 import FilterButtons from "../components/FilterButtons";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
@@ -11,7 +12,7 @@ function PageHome({ sort = "popular" }) {
   const [movies, setMovies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-  const ref = useRef(0);
+  const ref = useRef(null);
   const firstRenderRef = useRef(true);
 
   useEffect(() => {
@@ -33,30 +34,17 @@ function PageHome({ sort = "popular" }) {
       let data = await response.json();
       setMovies(data.results.slice(0, 12));
       setTotalPages(Math.min(50, data.total_pages));
-      console.log(data);
-        // if (currentPage !== 1) {
-        //   window.scrollTo({
-        //     top: ref.current.clientHeight,
-        //     left: 0,
-        //     behavior: "smooth",
-        //   });
-        // }
     };
 
     fetchMovies();
   }, [sort, currentPage]);
 
-
   useEffect(() => {
-      // if it is not the first render, we scroll to below the header
-      if (!firstRenderRef.current) {
-          window.scrollTo({
-            top: ref.current.clientHeight,
-            left: 0,
-            behavior: "smooth",
-          });
+    //   // if it is not the first render, we scroll to below the header
+      if ( ref.current && !firstRenderRef.current) {
+         ref.current.scrollIntoView({ behavior: "smooth" });
       }
-    // set firstRender.current to false
+    // // set firstRender.current to false
     firstRenderRef.current = false;
   }, [currentPage]);
 
@@ -71,11 +59,12 @@ function PageHome({ sort = "popular" }) {
 
   return (
     <main>
-      <header ref={ref}>
+      <header>
         <MovieBanner movies={movies} />
       </header>
 
-      <section>
+      <section  ref={ref}>
+        <FilterMobile />
         <FilterButtons />
         <MovieCard movies={movies} />
         <Stack spacing={2}>
