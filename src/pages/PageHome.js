9e9a0fbc2e7e8191538b422f74fa4e314087cec7
bChen-95 <bchen72@my.bcit.ca>
2023-05-13 10:14:48 -0700

    Revert "removed filtertest"
    This reverts commit 58bfb16ddc64d4abcdc5a05aea2c02130b63b38b.

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -3,6 +3,7 @@ import { appTitle, apiKey, API_TOKEN } from "../globals/globals";
 import MovieCard from "../components/MovieCard";
 import MovieBanner from "../components/MovieBanner";
 import FilterButtons from "../components/FilterButtons";
+import FilterTest from "../components/FilterTest";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 
@@ -52,6 +53,7 @@ function PageHome({ sort = "popular" }) {
       </header>
 
       <section>
+        <FilterTest />
         <FilterButtons />
         <MovieCard movies={movies} />
         <Stack spacing={2}>
