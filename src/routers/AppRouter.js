d8a833517e7e8a3b766a96fffa12d6d6c1e2f958
Crystal Chen <cchen435@my.bcit.ca>
2023-05-15 15:20:42 -0700

    fixed home page routing

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -19,7 +19,7 @@ function AppRouter() {
       <div className="wrapper">
         <Header/>
             <Routes>
-                <Route path="/" exact element={<PageHome/>}  />
+                <Route path="/movie-database" exact element={<PageHome/>}  />
                 <Route path="/about" element={<PageAbout />} />
                 <Route path="/favourites" element={<PageFavourite />} />
                 <Route path="/search/:userInput" element={<PageSearch />} />
