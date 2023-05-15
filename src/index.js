43c7cd97e0ecf8a275f4b7921ad70145be97da08
Brice Chen <bchen72@my.bcit.ca>
2023-05-15 14:23:30 -0700

    deployment steps for project

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './scss/styles.scss';
-import  AppRouter  from './routers/AppRouter';
-import { store } from './store/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./scss/styles.scss";
+import AppRouter from "./routers/AppRouter";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import { APP_FOLDER_NAME } from "./globals/globals";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppRouter />
+      <AppRouter basename={`/${APP_FOLDER_NAME}`} />
     </Provider>
   </React.StrictMode>
 );
-
-
