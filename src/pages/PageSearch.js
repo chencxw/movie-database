c318261ed3067b70c001bb3b85238c9f9f1017ee
Brice Chen <bchen72@my.bcit.ca>
2023-05-14 22:28:55 -0700

    added number of results to search page

--- src/pages/PageSearch.js
+++ src/pages/PageSearch.js
@@ -1,48 +1,60 @@
-import {useParams} from 'react-router-dom';
-import { appTitle, API_TOKEN  } from '../globals/globals';
-import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import { appTitle, API_TOKEN } from "../globals/globals";
+import { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
 
 function PageSearch() {
-    const {userInput} = useParams();
-    const baseUrl = 'https://api.themoviedb.org/3/search/movie?query=';
-    const [results, setResults] = useState();
+  const { userInput } = useParams();
+  const baseUrl = "https://api.themoviedb.org/3/search/movie?query=";
+  const [results, setResults] = useState();
 
-    // https://api.themoviedb.org/3/search/movie?query=john%20wick&api_key=1e55f4bc336e25a4e8d5e6a0873de6c6&language=en-US&page=1&include_adult=false
+  // https://api.themoviedb.org/3/search/movie?query=john%20wick&api_key=1e55f4bc336e25a4e8d5e6a0873de6c6&language=en-US&page=1&include_adult=false
 
-    useEffect(() => {
-        const fetchResults = async () => {
-            const response = await fetch(`${baseUrl}${userInput}&language=en-US&page=1`, {  
-              headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-              'Authorization': 'Bearer ' + API_TOKEN
-            }});
-            let searchData = await response.json();
-      
-            setResults(searchData);
+  useEffect(() => {
+    const fetchResults = async () => {
+      const response = await fetch(
+        `${baseUrl}${userInput}&language=en-US&page=1`,
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + API_TOKEN
           }
-        
-        fetchResults();
-    }, [userInput]);
+        }
+      );
+      let searchData = await response.json();
 
-    useEffect(() => {
-      document.title = `${appTitle} | Search`;
-    }, []);
+      setResults(searchData);
+    };
+
+    fetchResults();
+  }, [userInput]);
+
+  useEffect(() => {
+    document.title = `${appTitle} | Search`;
+  }, []);
 
   return (
-
-
     <section className="search-page">
-        <div className="search-title">
-            <h2>Search results for...</h2>
-            <h3>"{userInput}"</h3>
+      <div className="search-title">
+        {results ? results.results.length : "Search"} results for...{" "}
+        <h3>"{userInput}"</h3>
+      </div>
+      {/* {results ? <MovieCard movies={results.results} /> : <div className='loadingMovies'><p>Loading...</p></div>} */}
+      {results ? (
+        results.results.length > 0 ? (
+          <MovieCard movies={results.results} />
+        ) : (
+          <div className="no-result">
+            <p>No Movies Found.</p>
+          </div>
+        )
+      ) : (
+        <div className="loadingMovies">
+          <p>Loading...</p>
         </div>
-        {/* {results ? <MovieCard movies={results.results} /> : <div className='loadingMovies'><p>Loading...</p></div>} */}
-        {results ? (results.results.length > 0 ? <MovieCard movies={results.results} /> : <div className='no-result'><p>No Movies Found.</p></div>) : <div className='loadingMovies'><p>Loading...</p></div>}
-        
+      )}
     </section>
-    
-  )
+  );
 }
-export default PageSearch
+export default PageSearch;
